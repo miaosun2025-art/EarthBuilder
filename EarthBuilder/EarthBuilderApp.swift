fa7af399146ef70cbacbaf3fef96bdaf8b9681ee
Mia <miaosun2025@gmail.com>
2026-01-03 16:45:33 +0800

    Initial Commit

---
+++ EarthBuilder/EarthBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthBuilderApp.swift
+//  EarthBuilder
+//
+//  Created by miao on 2026/1/3.
+//
+
+import SwiftUI
+
+@main
+struct EarthBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
